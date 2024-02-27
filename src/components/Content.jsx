d7a5362c39d7530675b001cc06326d55fd901880
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-02-27 10:09:09 +0200

    1.3 Modify the variable definitions of the App

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,18 +1,11 @@
 import Part from "./Part";
 
-const Content = ({
-  part1,
-  part2,
-  part3,
-  exercises1,
-  exercises2,
-  exercises3,
-}) => {
+const Content = ({ part1, part2, part3 }) => {
   return (
     <div>
-      <Part name={part1} count={exercises1} />
-      <Part name={part2} count={exercises2} />
-      <Part name={part3} count={exercises3} />
+      <Part name={part1.name} count={part1.exercises} />
+      <Part name={part2.name} count={part2.exercises} />
+      <Part name={part3.name} count={part3.exercises} />
     </div>
   );
 };
